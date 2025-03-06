16a178166e4e9765b9a69302c20dd92b0061cef0
LAXMIKANTHREDDY92 <118453913+LAXMIKANTHREDDY92@users.noreply.github.com>
2025-03-06 17:29:39 +0530

    Add files via upload

---
+++ Day6/Pages/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CustomerApp1.Models;
+
+namespace CustomerApp1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CustomerApp1.Models.CustomerDbContext _context;
+
+        public CreateModel(CustomerApp1.Models.CustomerDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Customers.Add(Customer);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
