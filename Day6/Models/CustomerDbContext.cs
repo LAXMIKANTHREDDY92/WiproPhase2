498abfa7eeb4f6e19ca03ea53f1d939dcb5f23ac
LAXMIKANTHREDDY92 <118453913+LAXMIKANTHREDDY92@users.noreply.github.com>
2025-03-06 17:27:45 +0530

    Add files via upload
    performed customer CRUD operations using ASP.Net Core  Web App using razor Pages

---
+++ Day6/Models/CustomerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CustomerApp1.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CustomerApp1.Models
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
