beb19c6c1e4a9e5bd275841a2b4f85b21c076c9b
LAXMIKANTHREDDY92 <118453913+LAXMIKANTHREDDY92@users.noreply.github.com>
2025-03-10 17:23:01 +0530

    handson&asignment

---
+++ Day9/WebApi/Program.cs
@@ -0,0 +1,22 @@
+using BatchAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
